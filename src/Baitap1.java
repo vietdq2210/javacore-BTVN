74505046c9e414d0c6216c22c6d9b2fec3c2c810
vietdq2210 <vietdoan221099@gmail.com>
2022-06-20 16:48:59 +0700

    commit

--- src/Baitap1.java
+++ src/Baitap1.java
@@ -1,3 +1,65 @@
+import java.util.Scanner;
+
+//Viết một chương trình nhận vào 1 trong 12 tháng làm input. Output là số ngày trong tháng từ input. Giả sử tháng 2 luôn chỉ có 28 ngày. Yêu cầu làm theo 2 cách:
+//        Cách 1: Sử dụng enum và cấu trúc switch.
+//        Cách 2: Không sử dụng enum.
 public class Baitap1 {
+    private final static Scanner in = new Scanner(System.in);
+    public enum Month {
+        January(31),February(28),March(31),April(30),May(31),June(30),July(31),
+        August(31), September(30),October(31),November(30),December(31);
+        private int day;
+        private Month(int day){
+            this.day = day;
+        }
+    }
+
+    public static int checkInputMonth(){
+        while (true) {
+            try {
+                int input = Integer.parseInt(in.next().trim());
+                if (input >= 1 && input <= 12) {
+                    return input;
+                } else {
+                    System.err.println("Nhap thang sai");
+                    System.out.print("Nhap lai thang: ");
+                }
+            } catch (NumberFormatException ex) {
+                System.err.println("Nhap so thang tu 1 den 12");
+                System.out.print("Nhap lai thang: ");
+            }
+        }
+    }
+    public static void cach1(int input) {
+        Month m = Month.values()[input-1];
+        switch (m){
+            case January,February,March,April,May,June,July,August,September,October,November,December:
+                System.out.println(m+ " has the number of days: " + m.day);
+        }
+    }
+
+    public static void cach2(int input) {
+        switch (input){
+            case 1,3,5,7,8,10,12:
+                System.out.println("Thang " + input + " co so ngay la: 31 ngay");
+                break;
+            case 2:
+                System.out.println("Thang " + input + " co so ngay la: 28 ngay");
+                break;
+            case 4,6,9,11:
+                System.out.println("Thang " + input + " co so ngay la: 30 ngay");
+                break;
+            default:
+                System.out.println("Nhap thang khong hop le");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Nhap thang: ");
+        int input = checkInputMonth();
+        cach1(input);
+        cach2(input);
+    }
+
 
 }
--- src/Baitap2.java
+++ src/Baitap2.java
@@ -1,2 +1,57 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+//Viết chương trình tính ra số ngày giữa 2 mốc thời điểm người dùng nhập vào. Dữ liệu nhập vào của người dùng
+//        sẽ có dạng y1 m1 d1 y2 m2 d2. Với điều kiện mốc thời gian 1 lớn hơn mốc thời gian 2
+
 public class Baitap2 {
+
+    private final static Scanner in = new Scanner(System.in);
+
+    public static String checkInputDate() {
+        while(true){
+            try{
+                String result = in.next().trim();
+                SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+                Date date = format.parse(result);
+                if(result.equalsIgnoreCase(format.format(date))){
+                    return result;
+                }else {
+                    System.err.println("Re-input");
+                }
+            }catch (NumberFormatException ex){
+                System.err.println("Re-input");
+            }catch (ParseException ex){
+                System.err.println("Re-input");
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        long time ;
+        long day = 0;
+        System.out.print("Nhap ngay thang nam theo dinh dang 'yyyy-MM-dd': ");
+        String input1 = Baitap2.checkInputDate();
+        System.out.print("Nhap ngay thang nam theo dinh dang 'yyyy-MM-dd': ");
+        String input2 = Baitap2.checkInputDate();
+        Date date1 = new SimpleDateFormat("yyyy-MM-dd").parse(input1);
+        Date date2 = new SimpleDateFormat("yyyy-MM-dd").parse(input2);
+        if(date1.after(date2)){
+            time = date1.getTime() - date2.getTime();
+            day = TimeUnit.MILLISECONDS.toDays(time);
+        }else if(date1.before(date2)){
+            time = date2.getTime() - date1.getTime();
+            day = TimeUnit.MILLISECONDS.toDays(time);
+        }else if(date1.equals(date2)){
+            day = 0;
+        }else{
+            System.out.println("Not found");
+        }
+        System.out.println("So ngay giua " + input1 + " va " + input2 + " la: " + day + " ngay");
+    }
+
+
 }
